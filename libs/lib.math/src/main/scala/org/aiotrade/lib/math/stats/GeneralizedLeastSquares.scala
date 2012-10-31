679d888bf8469132e25b5f597623d7595dd498fe
Caoyuan Deng <dcaoyuan@gmail.com>
2012-10-31 20:43:26 +0800

    All GeneralizedLeastSquares methods are static now.

--- libs/lib.math/src/main/scala/org/aiotrade/lib/math/stats/GeneralizedLeastSquares.scala
+++ libs/lib.math/src/main/scala/org/aiotrade/lib/math/stats/GeneralizedLeastSquares.scala
@@ -4,40 +4,17 @@ package org.aiotrade.lib.math.stats
  *
  * @author Caoyuan Deng
  */
-class GeneralizedLeastSquares(x: Array[Double], y: Array[Double], weight: Array[Double], order: Int) {
-  assert(x != null && y != null && weight != null && x.length >= 2 && 
-         x.length == y.length && x.length == weight.length && order >= 2, 
-         "Invald params: x.length(>=2)=%s, y.length(=x.length)=%s, order(>=2)=%s".format(x.length, y.length, order))
+object GeneralizedLeastSquares {
   
-  private var _coefficients: Array[Double] = _
-  
-  def this(x: Array[Double], y: Array[Double], order: Int) = {
-    this(x, y, Array.fill[Double](x.length)(1.0), order)
-  }
-
-  def coefficients: Array[Double] = {
-    if (_coefficients == null) {
-      compute()
-    }
-    _coefficients
-  }
-
-  def fit(x: Double): Double = {
-    if (_coefficients == null)
-      compute()
-    if (_coefficients == null)
-      return 0
+  def coefficients(x: Array[Double], y: Array[Double], order: Int, _weight: Array[Double] = null): Array[Double] = {
+    val weight = if (_weight == null) Array.fill[Double](x.length)(1.0) else _weight
     
-    var y = 0.0
-    var i = 0
-    while (i < _coefficients.length) {
-      y += math.pow(x, i) * _coefficients(i)
-      i += 1
-    }
-    y
-  }
+    assert(
+      x != null && y != null && weight != null && x.length >= 2 && 
+      x.length == y.length && x.length == weight.length && order >= 2, 
+      "Invald params: x.length(>=2)=%s, y.length(=x.length)=%s, order(>=2)=%s".format(x.length, y.length, order)
+    )
 
-  private def compute() {
     val s = Array.ofDim[Double]((order - 1) * 2 + 1)
     var i = 0
     while (i < s.length) {
@@ -71,12 +48,19 @@ class GeneralizedLeastSquares(x: Array[Double], y: Array[Double], weight: Array[
       i += 1
     }
     
-    _coefficients = GeneralizedLeastSquares.multiLinearEquationGroup(a, f)
+    multiLinearEquationGroup(a, f)
   }
-}
-
-object GeneralizedLeastSquares {
   
+  def fit(coefficients: Array[Double])(x: Double): Double = {
+    var y = 0.0
+    var i = 0
+    while (i < coefficients.length) {
+      y += math.pow(x, i) * coefficients(i)
+      i += 1
+    }
+    y
+  }
+
   /**
    * @param left factors
    * @param right outputs
@@ -157,24 +141,26 @@ object GeneralizedLeastSquares {
   
   // --- simple test
   def main(args: Array[String]) {
+    testCoefficients
+    testMultiLinear
+  }
+
+  private def testCoefficients() {
     val x1 = Array(187.1, 179.5, 157.0, 197.0, 239.4, 217.8, 227.1, 233.4, 242.0, 251.9, 230.0, 271.8)
     val y1 = Array( 25.4,  22.8,  20.6,  21.8,  32.4,  24.4,  29.3,  27.9,  27.8,  34.2,  29.2,  30.0)
-    val gls1 = new GeneralizedLeastSquares(x1, y1, 2)
-    val coefs1 = gls1.coefficients
+    val coefs1 = coefficients(x1, y1, 2)
     println("y = %s + %s * x".format(coefs1(0), coefs1(1))) // y = 3.412968396061506 + 0.10814137404983369 * x
     
     val x2 = Array[Double]( 2000,  2001,  2002,  2003,  2004,   2005,   2006,   2007,   2008)
     val y2 = Array[Double](37.84, 44.55, 45.74, 63.80, 76.67, 105.59, 178.48, 355.27, 409.92)
     val w2 = Array[Double]( 11.0,  12.0,  13.0,  14.0,  15.0,   16.0,   17.0,   18.0,   19.0)
-    val gls2 = new GeneralizedLeastSquares(x2, y2, w2, 2)
-    val coefs2 = gls2.coefficients
+    val coefs2 = coefficients(x2, y2, 2, w2)
     println("y = %s + %s * x".format(coefs2(0), coefs2(1)))
-    val ys2 = List(2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010) map (x => gls2.fit(x))
+    val ys2 = List[Double](2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010) map fit(coefs2)
     println(ys2)
   }
 
-  // --- simple test
-  def testMultiLinear(args: Array[String]) {
+  private def testMultiLinear() {
     val r = multiLinearEquationGroup(
       Array(
         Array(1, 2, 3, 1), 
